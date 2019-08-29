6516d07d2527993df09dfc7e1cf9f765a1501406
redallen <zallen@redhat.com>
2019-08-29 14:38:11 -0400

    Fork openshift-storybook

---
+++ doczrc.js
@@ -0,0 +1,13 @@
+// https://www.docz.site/docs/project-configuration
+// https://github.com/pedronauck/docz/issues/793
+export default {
+  menu: [
+    'Getting Started',
+    { name: 'CRUD Operations' },
+    { name: 'Views' },
+    { name: 'Indicators' },
+    { name: 'Metrics' },
+    { name: 'Navigation' },
+    { name: 'Misc' },
+  ]
+}
---
+++ src/gatsby-theme-docz/components/Logo/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import logo from './logo.png';
+
+export const Logo = () => <img style={{height: '64px'}}  src={logo} alt="Red Hat logo" />;
